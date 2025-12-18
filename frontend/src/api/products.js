d7399028d6a49ae747c8775e97473038d3682c8c
Eric V <eric@eevn.io>
2025-12-17 22:09:53 -0800

    Add product creation UI

--- frontend/src/api/products.js
+++ frontend/src/api/products.js
@@ -3,3 +3,7 @@ import { apiFetch } from "./client.js";
 export async function listProducts() {
   return apiFetch("/api/products", { method: "GET" });
 }
+
+export async function createProduct(data) {
+  return apiFetch("/api/products", { method: "POST", body: data });
+}
